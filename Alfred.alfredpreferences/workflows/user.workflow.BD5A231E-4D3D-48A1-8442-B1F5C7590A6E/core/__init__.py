03b89ed76dd915f41d7080a3418431d3df0a4b5f
Ari Porad <ariporad@ariporad.local>
2021-06-02 12:52:09 -0700

    feat: refactor everything except homebrew to zero-sh

--- Alfred.alfredpreferences/workflows/user.workflow.BD5A231E-4D3D-48A1-8442-B1F5C7590A6E/core/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-This module contains all primary functionality of the workflow.
-
-This module is wrapped by the top-level scripts, which are invoked by Alfred.
-"""
-
-from .render import render_login_notice, render_search_result, render_pending
-from .auth import get_token, set_token
-from .salesforce import search, aquire_token
